a750f71e0e2df9b5c14dde157786e8afecfe0400
Maillein <realshinya1999@gmail.com>
2022-07-31 00:44:04 +0900

    fishとbyobuの設定を取り込んだ

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -94,11 +94,17 @@ cmp.setup.cmdline(':', {
 -- Setup lspconfig.
 -- local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 require('lspconfig')['clangd'].setup {
-  -- capabilities = capabilities
+  capabilities = capabilities
 }
--- 
--- require('lspconfig')['texlab'].setup {}
--- 
--- require('lspconfig')['sumneko_lua'].setup {}
--- 
--- require('lspconfig')['rust_analyzer'].setup {}
+
+require('lspconfig')['texlab'].setup {
+
+}
+
+require('lspconfig')['sumneko_lua'].setup {
+
+}
+
+require('lspconfig')['rust_analyzer'].setup {
+
+}

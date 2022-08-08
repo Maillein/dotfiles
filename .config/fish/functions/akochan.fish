833c172bfa8e077f3de0f37d3c36af64ac2bf5df
Maillein <realshinya1999@gmail.com>
2022-08-09 03:37:40 +0900

    akochanの設定を変更

--- .config/fish/functions/akochan.fish
+++ .config/fish/functions/akochan.fish
@@ -1,11 +1,15 @@
 function akochan
-    # set file_name /home/maillein/Downloads/(date '+%Y_%-m_%-d')_Jade_Room_South.json
-    set file_name /home/maillein/Downloads/(date '+%Y_%-m_%-d')_Gold_Room_South.json
-    docker run --rm --volume /home/maillein/Downloads:/home/maillein/Downloads akochan-reviewer:latest \
-        --no-open --use-placement-ev --without-viewer \
-        # -n 0.005 \
-        --in-file $file_name \
-        # -a (math ""(cat $file_name | jq -r '.name[]' | grep -n Maillein | string sub -s 1 -l 1)" - 1") -o - > ~/akochan-reviewer/report.html
-        --actor-name Maillein -o - > ~/akochan-reviewer/report.html
-    mv --backup=t $file_name /home/maillein/Downloads/検討済み牌譜/
+  set file_name /home/maillein/Downloads/(date '+%Y_%-m_%-d')_Jade_Room_South.json
+  # set file_name /home/maillein/Downloads/(date '+%Y_%-m_%-d')_Gold_Room_South.json
+  docker run --rm --volume /home/maillein/Downloads:/home/maillein/Downloads mjai-reviewer:latest \
+    -e akochan \
+    #--akochan-tactics ./akochan/setup_mjai.json \
+    --akochan-tactics ./tactics.json \
+    --player-name Maillein \
+    -i $file_name \
+    --no-open \
+    # --without-log-viewer \
+    -o - \
+    > ~/akochan-reviewer/report.html
+  mv --backup=t $file_name /home/maillein/Downloads/検討済み牌譜/
 end

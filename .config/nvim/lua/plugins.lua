a3b83d862831f70f24a3491361fd7a2dcdc16c34
Maillein <12164614+Maillein@users.noreply.github.com>
2024-01-12 18:02:06 +0900

    telescopeの依存を更新

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -133,7 +133,7 @@ return {
   -- ファジーファインダー
   {
     "nvim-telescope/telescope-file-browser.nvim",
-    dependencies = { { "nvim-telescope/telescope.nvim", tag = "0.1.2" }, "nvim-lua/plenary.nvim" },
+    dependencies = { { "nvim-telescope/telescope.nvim" }, "nvim-lua/plenary.nvim" },
     config = function()
       require("telescope").load_extension "file_browser"
 
